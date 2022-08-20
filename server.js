75ed3512a5759e3735f32a65522e23fa2286c01c
Fawaz Adeniji <fawazadeniji123@gmail.com>
2022-08-20 08:29:55 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
