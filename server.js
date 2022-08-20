f95a786503d564b4f991536264b5a4e9f92dd28e
Fawaz Adeniji <fawazadeniji123@gmail.com>
2022-08-20 15:33:59 +0100

    made signin route

--- server.js
+++ server.js
@@ -8,9 +8,40 @@ app.use(cors())
 
 const { log } = console
 
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+}
+
 app.get('/', (req, res) => {
-  log(req.headers)
-  res.send('this is a test')
+  res.json(database.users)
+})
+
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json(database.users[0])
+  } else {
+    res.status(401).json('error logging in')
+  }
 })
 
 const PORT = process.env.PORT || 3000
